c37119b8f2f013aca2a77a9dc7e5de314a540125
kimh060612 <kimh060612@naver.com>
2024-08-21 23:42:36 +0900

    ENV Var edit

--- run_eval.sh
+++ run_eval.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export EGL_DEVICE_ID=1
+export EGL_DEVICE_ID=0
 python3 habitat_baselines/run.py --exp-config habitat_baselines/config/multinav/ppo_multinav.yaml --agent-type obj-recog --run-type eval
--- run_train.sh
+++ run_train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export EGL_DEVICE_ID=1
+export EGL_DEVICE_ID=0
 CUDA_VISIBLE_DEVICES=0 python3 -m habitat_baselines.run --exp-config habitat_baselines/config/multinav/ppo_exp_multinav.yaml --agent-type exp-multinav --run-type train
