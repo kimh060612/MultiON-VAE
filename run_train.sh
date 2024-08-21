63a67cb471e2a8cff494122e1bdb54192be11d47
kimh060612 <kimh060612@naver.com>
2024-08-21 17:15:13 +0900

    Env Var Added

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 -m habitat_baselines.run --exp-config habitat_baselines/config/multinav/ppo_exp_multinav.yaml --agent-type exp-multinav --run-type train
+CUDA_VISIBLE_DEVICES=0 python3 -m habitat_baselines.run --exp-config habitat_baselines/config/multinav/ppo_exp_multinav.yaml --agent-type exp-multinav --run-type train
