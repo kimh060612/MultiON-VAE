aa435238ba533d8b1d66cd1a9ef6ab8d504656e6
kimh060612 <kimh060612@naver.com>
2024-08-30 01:18:44 +0900

    EGL version change

--- run_train.sh
+++ run_train.sh
@@ -2,7 +2,7 @@
 
 export MAGNUM_LOG=verbose
 export MAGNUM_GPU_VALIDATION=ON
-export EGL_DEVICE_ID=1
+export EGL_DEVICE_ID=0
 # Exploration Training
 # CUDA_VISIBLE_DEVICES=0 python3 -m habitat_baselines.run --exp-config habitat_baselines/config/multinav/ppo_exp_multinav.yaml --agent-type exp-multinav --run-type train
 # Exploration Attention Training
